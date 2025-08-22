dd462902a60d5ce3205c1612951b9853d770beb0
doo1011 <1kdhur2@gmail.com>
2025-08-22 10:16:23 +0900

    INIT COMMIT

---
+++ src/main/java/org/fastcampus/jober/JoberServerApplication.java
@@ -0,0 +1,13 @@
+package org.fastcampus.jober;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoberServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoberServerApplication.class, args);
+    }
+
+}
