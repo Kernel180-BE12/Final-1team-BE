17991406fb8bf412de230fa2e47505ed7ca8adc1
yk <juyk.oung@gmail.com>
2025-08-29 18:56:48 +0900

    feat: 스페이스멤버 리파지토리 구현

--- src/main/java/org/fastcampus/jober/space/repository/SpaceMemberRepository.java
+++ src/main/java/org/fastcampus/jober/space/repository/SpaceMemberRepository.java
@@ -1,6 +1,9 @@
 package org.fastcampus.jober.space.repository;
 
+import org.fastcampus.jober.space.entity.SpaceMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SpaceMemberRepository extends JpaRepository {
+public interface SpaceMemberRepository extends JpaRepository<SpaceMember, Long> {
+
+
 }
