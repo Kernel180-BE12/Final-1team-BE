78496bd151ca1cd324a4beb87fdd798914f5c38f
yk <juyk.oung@gmail.com>
2025-08-29 19:00:18 +0900

    feat: 스페이스이름 검색 구현

--- src/main/java/org/fastcampus/jober/space/repository/SpaceRepository.java
+++ src/main/java/org/fastcampus/jober/space/repository/SpaceRepository.java
@@ -1,12 +1,11 @@
 package org.fastcampus.jober.space.repository;
 
-import org.fastcampus.jober.space.dto.request.SpaceRequestDto;
-import org.fastcampus.jober.space.dto.response.SpaceResponseDto;
 import org.fastcampus.jober.space.entity.Space;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface SpaceRepository extends JpaRepository<Space,Long> {
 
+    Optional<Space> findBySpaceName(String spaceName);
 }
