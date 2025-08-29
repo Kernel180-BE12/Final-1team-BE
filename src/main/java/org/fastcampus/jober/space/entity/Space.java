4cfac043ded79cb97a5a0e7b4459a5f135c963e4
yk <juyk.oung@gmail.com>
2025-08-29 18:47:05 +0900

    feat: 제약사항, dto변환 및 스페이스 업데이트 메서드 추가

--- src/main/java/org/fastcampus/jober/space/entity/Space.java
+++ src/main/java/org/fastcampus/jober/space/entity/Space.java
@@ -4,13 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
+import org.fastcampus.jober.space.dto.response.SpaceResponseDto;
 
 import java.time.LocalDate;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -18,13 +20,19 @@ public class Space {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+
+    @NotBlank(message = "스페이스 이름은 필수입니다.")
+    private String spaceName;
+
     private String adminName;
+
+    @Pattern(regexp = "^010-\\d{4}-\\d{4}$", message = "전화번호는 010-0000-0000 형식이어야 합니다.")
     private String adminNum;
+
+    private String spaceUrl;
     // 이 아래로 꼭 필요할지?
 //    private String faxNum;
 //    private String email;
-//    private String url;
 //    private String businessType;
 //    private String corporateRegistrationNo;
 //    private String businessRegistrationNo;
@@ -34,4 +42,19 @@ public class Space {
 //    private String businessItem;
 //    private String taxEmail;
 
+    public SpaceResponseDto toResponseDto() {
+        return SpaceResponseDto.builder()
+                .id(this.id)
+                .spaceName(this.spaceName)
+                .adminName(this.adminName)
+                .adminNum(this.adminNum)
+                .spaceUrl(this.spaceUrl)
+                .build();
+    }
+
+    public void updateSpaceInfo(String spaceName, String adminName, String adminNum) {
+        if (spaceName != null && !spaceName.isBlank()) {this.spaceName = spaceName;}
+        if (adminName != null && !adminName.isBlank()) {this.adminName = adminName;}
+        if (adminNum != null && !adminNum.isBlank()) {this.adminNum = adminNum;}
+    }
 }
