b94115670dfdcbca11118f0118ac22b939e258f7
juyeongkyeong <juyk.oung@gmail.com>
2025-08-31 22:56:07 +0900

    feat: 스페이스 조회 및 삭제 구현

--- src/main/java/org/fastcampus/jober/space/controller/SpaceController.java
+++ src/main/java/org/fastcampus/jober/space/controller/SpaceController.java
@@ -1,24 +1,23 @@
 package org.fastcampus.jober.space.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.fastcampus.jober.space.dto.response.SpaceGetResponseDtd;
+import org.fastcampus.jober.user.entity.Users;
+import org.springframework.security.core.Authentication;
 import org.fastcampus.jober.space.dto.request.SpaceCreateRequestDto;
-import org.fastcampus.jober.space.dto.request.SpaceMemberRequestDto;
-import org.fastcampus.jober.space.dto.request.SpaceRequestDto;
 import org.fastcampus.jober.space.dto.request.SpaceUpdateRequestDto;
-import org.fastcampus.jober.space.dto.response.SpaceMemberResponseDto;
 import org.fastcampus.jober.space.dto.response.SpaceResponseDto;
-import org.fastcampus.jober.space.repository.SpaceMemberRepository;
 import org.fastcampus.jober.space.service.SpaceService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Tag(name = "Spaces", description = "스페이스 관련 API")
 @Controller
@@ -39,6 +38,22 @@ public class SpaceController {
     }
 
     @Operation(
+            summary = "단일 스페이스 조회",
+            description = "ID를 통해 특정 스페이스의 상세 정보를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 스페이스 정보를 조회함",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = SpaceResponseDto.class))),
+            @ApiResponse(responseCode = "404", description = "해당 ID의 스페이스를 찾을 수 없음")
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<SpaceResponseDto> getSpace (@PathVariable Long id) {
+        SpaceResponseDto result = spaceService.getSpace(id);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+
+    @Operation(
             summary = "스페이스 수정",
             description = "스페이스 정보를 수정합니다. (권한이 있는 사용자만 가능)"
     )
@@ -53,6 +68,19 @@ public class SpaceController {
 
         SpaceResponseDto result = spaceService.updateSpace(id, dto, authentication);
         return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @Operation(summary = "스페이스 삭제",
+            description = "지정된 스페이스 ID를 통해 스페이스를 삭제합니다. 스페이스 관리자만 삭제할 수 있습니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "성공적으로 스페이스를 삭제함"),
+            @ApiResponse(responseCode = "403", description = "스페이스 삭제 권한이 없음"),
+            @ApiResponse(responseCode = "404", description = "해당 ID의 스페이스를 찾을 수 없음")
+    })
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSpace(@PathVariable Long id, Users user) {
+        spaceService.deleteSpace(id, user);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 //    @PostMapping("/{spaceId}/members")
