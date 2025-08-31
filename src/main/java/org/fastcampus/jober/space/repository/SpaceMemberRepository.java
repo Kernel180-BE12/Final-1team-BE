7eb47a1e0949337de2731debdd2c4d55dd907f44
juyeongkyeong <juyk.oung@gmail.com>
2025-08-31 23:09:58 +0900

    feat: 권한체크용 메서드 구현

--- src/main/java/org/fastcampus/jober/space/repository/SpaceMemberRepository.java
+++ src/main/java/org/fastcampus/jober/space/repository/SpaceMemberRepository.java
@@ -3,7 +3,11 @@ package org.fastcampus.jober.space.repository;
 import org.fastcampus.jober.space.entity.SpaceMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.lang.management.LockInfo;
+import java.util.Optional;
+
 public interface SpaceMemberRepository extends JpaRepository<SpaceMember, Long> {
 
+    Optional<SpaceMember> findBySpaceIdAndUserIdAndAuthority(Long spaceId, Long userId, String authority);
 
 }
