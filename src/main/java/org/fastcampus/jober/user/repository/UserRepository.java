2bedca294cebca20a563bb11b863057d85726c62
Kyeongdoo Hur <1kdhur2@naver.com>
2025-09-01 15:19:05 +0900

    회원가입, 로그인, 로그아웃 구현 (#5)
    * Create deploy.yml
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    * Update ci.yml
    
    * Update ci.yml
    
    * chore: docker 관련 설정
    
    * Update ci.yml
    
    * Update ci.yml
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    * Update deploy.yml
    
    * Update ci.yml
    
    * Update ci.yml
    
    * chore: profile 레벨 변경
    
    * feat: spring security 설정
    
    * feat: swagger try out 을 위한 쿠키 필터 추가
    
    * chore: spring security 의존성 추가
    
    * chore: swagger 쿠키 설정 추가
    
    * rename: 디렉토리 구조 잘못 설정된 부분 이동
    
    * feat: 팩토리 메서드 구현
    
    * feat: 회원가입, 로그인 관련 DTO 구현
    
    * feat: 비밀번호 해싱 구현
    
    * feat: UserDetailsService 구현
    
    * feat: 로그인, 회원가입, 로그아웃 구현
    
    * chore: 미사용 코드 주석 및 제거
    
    * chore: security test 의존성 추가
    
    * test: 회원가입, 로그인, 로그아웃 테스트 코드 작성
    
    * feat: 세션을 세션 레지스트리에 저장하도록 변경
    
    * chore: thymeleaf 의존성 추가
    
    * feat: session 모니터링 뷰 추가
    
    * fix: 세션 모니터링 url csrf, filter 통과 허용
    
    * fix: 로그아웃 시에 sessionRegistry 에 등록된 세션 삭제하도록 수정
    
    ---------
    
    Co-authored-by: doo1011 <1kdhur2@gmail.com>

--- src/main/java/org/fastcampus/jober/user/repository/UserRepository.java
+++ src/main/java/org/fastcampus/jober/user/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package org.fastcampus.jober.user.repository;
 
-public interface UserRepository {
+import org.fastcampus.jober.user.entity.Users;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<Users, Long> {
+    Optional<Users> findByUsername(String username);
+    boolean existsByUsername(String username);
 }
---
+++ src/main/java/org/fastcampus/jober/util/PasswordHashing.java
@@ -0,0 +1,23 @@
+package org.fastcampus.jober.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordHashing {
+    private static PasswordEncoder encoder;
+
+    @Autowired
+    public PasswordHashing(PasswordEncoder passwordEncoder) {
+        PasswordHashing.encoder = passwordEncoder;
+    }
+
+    public static String hash(String rawPassword) {
+        return encoder.encode(rawPassword);
+    }
+
+//    public static boolean matches(String rawPassword, String encoded) {
+//        return encoder.matches(rawPassword, encoded);
+//    }
+}
