8e110c7d7fa66fa08db7400155d609becca8ed35
yk <juyk.oung@gmail.com>
2025-09-01 11:50:24 +0900

    refactor: toEntity ignore 추가

--- src/main/java/org/fastcampus/jober/space/mapper/SpaceMapper.java
+++ src/main/java/org/fastcampus/jober/space/mapper/SpaceMapper.java
@@ -2,7 +2,6 @@ package org.fastcampus.jober.space.mapper;
 
 import org.fastcampus.jober.space.dto.request.SpaceCreateRequestDto;
 import org.fastcampus.jober.space.dto.request.SpaceUpdateRequestDto;
-import org.fastcampus.jober.space.dto.response.SpaceGetResponseDtd;
 import org.fastcampus.jober.space.dto.response.SpaceResponseDto;
 import org.fastcampus.jober.space.entity.Space;
 import org.mapstruct.*;
@@ -14,7 +13,10 @@ public interface SpaceMapper {
     SpaceMapper INSTANCE = Mappers.getMapper(SpaceMapper.class);
 
     // 생성용: DTO → Entity
-    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "spaceId", ignore = true)
+    @Mapping(target = "admin", ignore = true)
+    @Mapping(target = "spaceUrl", ignore = true)
+    @Mapping(target = "spaceMembers", ignore = true)
     Space toEntity(SpaceCreateRequestDto dto);
 
     // 조회용: Entity → DTO
@@ -36,5 +38,4 @@ public interface SpaceMapper {
         }
     }
 
-    SpaceGetResponseDtd toGetResponseDto(Space space);
 }
