a8646e58f1f6945c0d9e6e4d19a9bcd9060a9d40
yk <juyk.oung@gmail.com>
2025-09-02 17:11:19 +0900

    feat: findByIdOrThrow 구현

--- src/main/java/org/fastcampus/jober/space/repository/SpaceRepository.java
+++ src/main/java/org/fastcampus/jober/space/repository/SpaceRepository.java
@@ -1,5 +1,7 @@
 package org.fastcampus.jober.space.repository;
 
+import org.fastcampus.jober.error.BusinessException;
+import org.fastcampus.jober.error.ErrorCode;
 import org.fastcampus.jober.space.entity.Space;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +10,9 @@ import java.util.Optional;
 public interface SpaceRepository extends JpaRepository<Space,Long> {
 
     Optional<Space> findBySpaceName(String spaceName);
+
+
+default Space findByIdOrThrow(Long id) {
+    return findById(id).orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND, "존재하지 않는 스페이스입니다."));
+}
 }
