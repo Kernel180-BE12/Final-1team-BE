e6842f0784e7d6f4e39dc36aaeacd2f2c1eb30f2
doo1011 <1kdhur2@gmail.com>
2025-09-03 16:15:24 +0900

    chore: 미사용 코드 주석 처리

--- src/main/java/org/fastcampus/jober/space/repository/SpaceRepository.java
+++ src/main/java/org/fastcampus/jober/space/repository/SpaceRepository.java
@@ -5,7 +5,6 @@ import org.fastcampus.jober.error.ErrorCode;
 import org.fastcampus.jober.space.entity.Space;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface SpaceRepository extends JpaRepository<Space,Long> {
 
