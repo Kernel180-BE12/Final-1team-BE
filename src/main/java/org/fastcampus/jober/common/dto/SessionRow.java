04b41b0f30fb81c9bd893ee38054dfb10c258d7d
doo1011 <1kdhur2@gmail.com>
2025-09-11 17:35:51 +0900

    feat: session 모니터링 화면에 유효시간, 만료여부 추가

--- src/main/java/org/fastcampus/jober/common/dto/SessionRow.java
+++ src/main/java/org/fastcampus/jober/common/dto/SessionRow.java
@@ -7,5 +7,8 @@ public record SessionRow(
         String authorities,
         String sessionId,
         ZonedDateTime lastRequestLocal,
-        String ago
+        String ago,
+        boolean expired,                 // 동시로그인 만료 여부
+        String remaining,                // 남은 유효시간 (사람 친화적)
+        ZonedDateTime expiresAtLocal     // 만료 예정 시각
 ) {}
