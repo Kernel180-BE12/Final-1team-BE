477d505a9a944a9bc1067582fb8dc4bdd1403ea4
doo1011 <1kdhur2@gmail.com>
2025-09-12 18:33:31 +0900

    fix: warning 해결

--- src/main/java/org/fastcampus/jober/config/SecurityConfig.java
+++ src/main/java/org/fastcampus/jober/config/SecurityConfig.java
@@ -27,7 +27,6 @@ import org.springframework.security.crypto.bcrypt.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.*;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
