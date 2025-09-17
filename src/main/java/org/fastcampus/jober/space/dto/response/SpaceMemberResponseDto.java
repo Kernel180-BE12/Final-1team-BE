4a2a3437f8209001f9bc38fb038504ae31bf94e8
yk <juyk.oung@gmail.com>
2025-09-17 15:02:50 +0900

    refactor: 스페이스생성시 관리자 권한부여 구현

--- src/main/java/org/fastcampus/jober/space/dto/response/SpaceMemberResponseDto.java
+++ src/main/java/org/fastcampus/jober/space/dto/response/SpaceMemberResponseDto.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.fastcampus.jober.space.entity.Authority;
 
 @Getter
 @NoArgsConstructor
@@ -15,14 +16,11 @@ public class SpaceMemberResponseDto {
   private Long id;
 
   @Schema(description = "구성원 권한", example = "ADMIN")
-  private String authority;
+  private Authority authority;
 
   @Schema(description = "구성원 태그", example = "마케팅팀")
   private String tag;
 
   @Schema(description = "사용자 ID", example = "2002")
   private Long userId;
-
-  @Schema(description = "스페이스 ID", example = "10")
-  private Long spaceId;
 }
